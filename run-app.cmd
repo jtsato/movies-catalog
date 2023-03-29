c12253086a4422a58f7125d93ffe81b5de120100
jtsato <jorge.takeshi.sato@gmail.com>
2023-03-29 17:21:38 -0300

    refactor(app): refactoring command line batchs & incremente springboot version

--- run-app.cmd
+++ run-app.cmd
@@ -4,7 +4,9 @@ FOR /f %%i IN ('dir target /s /b') DO RD /s /q %%i
 CLS
 
 ECHO.
-CALL mvn clean install -Dmaven.test.skip=true
+FOR /f %%i in ('DIR %HOME%\.m2\repository\io\github\jtsato\moviescatalog* /b') do RD /s /q %%i
+CALL mvn -e clean compile
+CALL mvn -e clean install -Dmaven.test.skip=true
 
 ECHO.
 CALL mvn spring-boot:run -Dspring-boot.run.jvmArguments='-Dspring.profiles.active=test' -f ./configuration
